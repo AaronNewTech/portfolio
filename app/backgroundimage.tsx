22895ae4a010356e2ada2906006f94bb18adeab3
Aaron <arnhsmith@gmail.com>
2023-10-13 18:07:02 -0400

    created background image layouts

---
+++ app/backgroundimage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BackgroundImage() {
+  return (
+    <div className="relative h-screen bg-cover bg-fixed" style={{ backgroundImage: 'url("/images/homepage1/home-page-1-background.png")' }}>
+      
+    </div>
+  );
+};
+
