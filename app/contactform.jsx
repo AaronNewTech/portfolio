05617665e6105ff49e4b9743eecd55419a2b37b1
Aaron <arnhsmith@gmail.com>
2023-10-16 16:51:00 -0400

    added media queries for mobile screens

--- app/contactform.jsx
+++ app/contactform.jsx
@@ -1,106 +1,107 @@
-"use client"; 
-import React from 'react'
-import { useRef, useState } from 'react'
-import emailjs from '@emailjs/browser'
-
-// service_l7rkcwr
-// template_k6nu8zm
-// API : LJ4mLT8Pe9hR-lLgy
+"use client";
+import React from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  const formRef = useRef()
+  const formRef = useRef();
   const [form, setForm] = useState({
-    name:'',
-    email:'',
-    message:'',
-  })
-  const [loading, setLoading] = useState(false)
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setForm({...form, [name]: value})
-  }
+    setForm({ ...form, [name]: value });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    emailjs.send("service_l7rkcwr",
-    "template_k6nu8zm",
-    {
-      from_name:form.name,
-      to_name:'AaronSmith',
-      from_email:form.email,
-      to_email:'arnhsmith@gmail.com',
-      message: form.message
-    }, 
-    "LJ4mLT8Pe9hR-lLgy"
-    )
-    .then(() => {
-      setLoading(false)
-      alert('Thank You for your interest. I will contact you soon.');
-      setForm({
-        name:"",
-        email:"",
-        message:""
-      })
-    }, (error) => {setLoading(false),
-      console.log(error);
-      alert('Something went wrong')
-    }
-    )
-  }
-  
+    emailjs
+      .send(
+        "service_l7rkcwr",
+        "template_k6nu8zm",
+        {
+          from_name: form.name,
+          to_name: "AaronSmith",
+          from_email: form.email,
+          to_email: "arnhsmith@gmail.com",
+          message: form.message,
+        },
+        "LJ4mLT8Pe9hR-lLgy"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank You for your interest. I will contact you soon.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false), console.log(error);
+          alert("Something went wrong");
+        }
+      );
+  };
+
   return (
-    <div id='contact-form' className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
-        
-        <form 
+    <div
+      id="contact-form"
+      className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"
+    >
+      <form
         ref={formRef}
         onSubmit={handleSubmit}
-        className=' form  flex flex-col '><h1 id="contact-text">CONTACT ME</h1>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium '>Name</span>
-            <input
-            type='text'
-            name='name'
+        className=" form  flex flex-col "
+      >
+        <h1 id="contact-text">CONTACT ME</h1>
+        <label className="flex flex-col">
+          <span className="text-white font-medium ">Name</span>
+          <input
+            type="text"
+            name="name"
             value={form.name}
             onChange={handleChange}
             placeholder="Enter your name"
-            className='bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium'
-           />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Email</span>
-            <input
-            type='email'
-            name='email'
+            className="bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="text-white font-medium mb-4">Email</span>
+          <input
+            type="email"
+            name="email"
             value={form.email}
             onChange={handleChange}
             placeholder="Enter your email"
-            className='bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium'
-           />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Message</span>
-            <textarea
+            className="bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="text-white font-medium mb-4">Message</span>
+          <textarea
             // rows="7"
-            name='message'
+            name="message"
             value={form.message}
             onChange={handleChange}
             placeholder="Enter your message"
-            className='bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium'
-           />
-          </label>
-          <button
-  type='submit'
-  className='bg-blue-500 py-2 px-4 rounded-lg text-white font-bold shadow-md shadow-primary outline-none w-fit transition duration-300 transform hover:scale-105'
->
-  {loading ? 'Sending...' : 'Send'}
-</button>
-        </form>
-     
-
-      
+            className="bg-tertiary py-2 px-6 placeholder:text-secondary text-black rounded-lg outline-none border-none font-medium"
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-blue-500 py-2 px-4 rounded-lg text-white font-bold shadow-md shadow-primary outline-none w-fit transition duration-300 transform hover:scale-105"
+        >
+          {loading ? "Sending..." : "Send"}
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import "./styles.css";
 export default function Home() {
   return (
     <main id="main">
-     <div id="navbar-container" >
+      <div id="navbar-container">
         <NavBar />
-    </div>
+      </div>
       <div className="homepage-1">
         <img
           id="homepage-image"
@@ -121,10 +121,9 @@ export default function Home() {
       </div>
 
       <div className="homepage-5">
-       
-          <div>
-            <ContactForm />
-          
+        <div>
+          <ContactForm />
+
           <img id="contact-me" src="/images/contact/22.png" />
           <div>
             <img
@@ -135,180 +134,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      {/* src="/images/contact/26.png */}
-      {/* <div>
-        <NavBar />
-        <div className="w-full h-full relative">
-          <div
-            style={{
-              ...sectionStyle,
-              backgroundImage: `url('/images/homepage1/home-page-1-background.png')`,
-            }}
-          >
-            <div className="absolute bottom-10 left-20 w-1/3 flex">
-              <div className="mr-4 mb-20">
-                <Link href="/#about-me">
-                  <Image
-                    src="/images/homepage1/about-me.png"
-                    alt="About Me Button"
-                    width={400}
-                    height={100}
-                    z-index={10}
-                  />
-                </Link>
-              </div>
-              <div>
-                <Link href="/#projects">
-                  <Image
-                    src="/images/homepage1/projects.png"
-                    alt="Projects Button"
-                    width={400}
-                    height={100}
-                    z-index={10}
-                  />
-                </Link>
-              </div>
-            </div>
-            <div className="absolute bottom-40 right-5 w-1/2 flex">
-              <div>
-                <Image
-                  src="/images/homepage1/computer.png"
-                  alt="About Me Button"
-                  width={800}
-                  height={800}
-                  z-index={10}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div id="about-me" className="w-full h-full relative">
-          <div
-            style={{
-              ...sectionStyle,
-              backgroundImage: `url('/images/about/about-me-image.png')`,
-            }}
-          ></div>
-        </div>
-
-        <div id="experience" className="w-full h-full relative">
-          <div
-            style={{
-              ...sectionStyle,
-              backgroundImage: `url('/images/about/experience-page.png')`,
-            }}
-          ></div>
-        </div>
-
-        <div id="projects" className="w-full h-full relative">
-          <div
-            style={{
-              ...sectionStyle,
-              backgroundImage: `url('/images/homepage2/12.png')`,
-            }}
-          >
-            <div className="absolute bottom-0 left-0 w-1/1 flex">
-              <div>
-                <Image
-                  src="/images/homepage2/15.png"
-                  alt="react-logo"
-                  layout="responsive"
-                  width={400}
-                  height={100}
-                  z-index={10}
-                />
-              </div>
-              <div>
-                <Image
-                  src="/images/homepage2/16.png"
-                  alt="javascript-logo"
-                  layout="responsive"
-                  width={400}
-                  height={100}
-                  z-index={10}
-                />
-              </div>
-              <div>
-                <Image
-                  src="/images/homepage2/17.png"
-                  alt="css-logo"
-                  layout="responsive"
-                  width={400}
-                  height={100}
-                  z-index={10}
-                />
-              </div>
-              <div>
-                <Image
-                  src="/images/homepage2/18.png"
-                  alt="html-logo"
-                  layout="responsive"
-                  width={400}
-                  height={100}
-                  z-index={10}
-                />
-              </div>
-              <div>
-                <Image
-                  src="/images/homepage2/19.png"
-                  alt="tailwind-logo"
-                  layout="responsive"
-                  width={400}
-                  height={100}
-                  z-index={10}
-                />
-              </div>
-            </div>
-            <div className="absolute bottom-40 flex">
-              <div>
-                <Link href="https://www.loom.com/share/41e418e86c974b9385b94bf251bba392">
-                  <Image
-                    src="/images/homepage2/13.png"
-                    alt="speech-trainer-project"
-                    layout="responsive"
-                    width={800}
-                    height={800}
-                    z-index={10}
-                  />
-                </Link>
-              </div>
-              <div className="">
-                <Link href="https://www.loom.com/share/38c4a54a33fa4ffc826e04214d7fbd3d">
-                  <Image
-                    src="/images/homepage2/14.png"
-                    alt="cocktail-creator-project"
-                    layout="responsive"
-                    width={800}
-                    height={800}
-                    z-index={10}
-                  />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div id="contact-me" className="relative h-screen aspect-auto bg-cover bg-fixed bg-[url('/images/contact/22.png')] bg-no-repeat">
-          <div className="absolute bottom-40 right-5 w-1/2 flex">
-            <div className="">
-              <Link href="/#contact-me">
-                <Image
-                  src="/images/contact/26.png"
-                  alt="About Me Button"
-                  width={800}
-                  height={800}
-                  z-index={10}
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="absolute bottom-20 left-20 w-1/4 flex">
-            <ContactForm />
-          </div>
-        </div>
-      </div> */}
     </main>
   );
 }
